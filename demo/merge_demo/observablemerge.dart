664d721c5811aa471c986f8ad88747bf74c126be
John Evans <prujohn@gmail.com>
2012-01-29 16:28:44 -0600

    added named parameters to the merge demo.

--- demo/merge_demo/observablemerge.dart
+++ demo/merge_demo/observablemerge.dart
@@ -21,15 +21,15 @@ class observablemerge {
   void run() {
   
     var o1 = Observable
-    .randomInt(0, 100, 1, 1000, 5)
+    .randomInt(0, 100, intervalLow:1, intervalHigh:1000, howMany:20)
     .apply((v) => 'Observable 1: $v');
 
     var o2 = Observable
-    .randomInt(0, 100, 1, 1000, 20)
+    .randomInt(0, 100, intervalLow:1, intervalHigh:1000, howMany:20)
     .apply((v) => 'Observable 2: $v');
     
     var o3 = Observable
-    .randomInt(0, 100, 1, 1000, 20)
+    .randomInt(0, 100, intervalLow:1, intervalHigh:1000, howMany:20)
     .apply((v) => 'Observable 3: $v');
     
     o1
